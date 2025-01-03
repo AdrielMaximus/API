297c9ded688bc775384619d737cd9b66902a2140
Adriel Maximus <adrvlogs885@gmail.com>
2025-01-03 19:32:03 -0300

    API MADE

--- app.py
+++ app.py
@@ -23,16 +23,31 @@ meals = [
 
 
 #localhost/meals (all meals)
-@app.route('/meals')
-def all_books():
+@app.route('/meals', methods=['GET'])
+def all_meals():
     return jsonify(meals)
 
 #localhost/meal/id (one single meal)
+@app.route('/meals/<int:id>', methods=['GET'])
+def single_meals(id):
+    for meal in meals:
+        if meal.get('id') == id:
+            return jsonify(meal)
+
 #localhost/meal/id (change a meal)
+@app.route('/meals/<int:id>', methods=['PUT'])
+def change_meals(id):
+    changed_meal=request.get_json()
+    for index, meal in enumerate(meals):
+        if meal.get('id') == id:
+            meals[index].update(changed_meal)
+            return jsonify(meals[index])
 #localhost/meal/id (delete a meal)
-#Create (Post)
-#Read (GET)
-#Update (PUT/FETCH)
-#DELETE (DELETE)
+@app.route('/meals/<int:id>', methods=['DELETE'])
+def delete_meal(id):
+    for index, meal in enumerate(meals):
+        if meal.get('id') == id:
+            del meals[index]
+    return jsonify(meals)
 
 app.run(port=5000, host='localhost', debug=True)
